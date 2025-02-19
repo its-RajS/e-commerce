3c3aa3c22a96395e25ec09f22c1236b00ab854af
Raj <rajsharma222547@gmail.com>
2025-02-19 13:43:51 +0530

    api and Navigation setup in the Store

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,8 @@
-import react from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Route, RouterProvider, createRoutesFromElements } from "react-router";
 import { createBrowserRouter } from "react-router-dom";
+import "./index.css";
 
 const router = createBrowserRouter(
   createRoutesFromElements(<Route path="/" element={<App />} />)
